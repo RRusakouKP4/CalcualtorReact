1b37a67b8985809176b4d4543e584fbf9e9a9d1e
RRusakouKP4 <rusakou.roman@gmail.com>
2022-11-10 20:56:52 +0300

    Initial commit
    React,express installed

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+function App(){
+  return (
+    <div className = "App">
+      <Box>
+        <h1>Test String</h1>
+      </Box>
+    </div>
+  )
+}
+
+export default App;
